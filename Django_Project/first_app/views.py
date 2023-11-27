48c94eed2180475ac2dd956e545edf2ace390be4
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-11-27 02:54:35 +0200

    feat: homepage, todos, database implementation

--- Django_Project/first_app/views.py
+++ Django_Project/first_app/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import TodoItem
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def todo_list(request):
+    todo_items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": todo_items})
